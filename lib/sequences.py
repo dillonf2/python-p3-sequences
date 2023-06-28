9c3d88832d3ac1752e79cf92ee08d604c45d55ec
Frank Dillon <dillonf2@outlook.com>
2023-06-28 16:19:52 -0400

    python sequences lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+        return
+
+    fibonacci_list = [0, 1] if length >= 2 else [0]
+
+    while len(fibonacci_list) < length:
+        next_number = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_number)
+
+    print(fibonacci_list)
